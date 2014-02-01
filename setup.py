1e8554e21fc0a1452b183cc512a32ae6297e613a
Neil Kandalgaonkar <neilk@brevity.org>
2014-01-31 17:17:57 -0800

    version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ from setuptools import setup
 
 setup(
     name = "rds-host",
-    version = "0.1.0",
+    version = "0.1.2",
     author = "Neil Kandalgaonkar",
     author_email = "neilk@neilk.net",
     description = "Get hostname from Amazon RDS instance name",
